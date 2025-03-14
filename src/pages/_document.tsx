4bfd861804f681bc5a1464d7959e748cdafac245
TakaakiNagayasu <dn2ri8+github@gmail.com>
2025-03-14 20:38:00 +0900

    html・headタグを明示的に指定

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import type { DocumentContext } from "next/document";
+import Document from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
